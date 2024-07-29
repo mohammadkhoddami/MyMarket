8928efd2418077b7e692219a689aac9f4a38a013
mohammadkhoddami <mohammadh.khoddami@gmail.com>
2024-07-29 18:29:58 +0330

    add product , product detail , models tempaltes views and urls !

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'home'
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home')
+    path('', views.HomeView.as_view(), name='home'),
+    path('product/<int:pk>/', views.ProductDetail.as_view(), name='product')
 ]
--- home/views.py
+++ home/views.py
@@ -3,7 +3,12 @@ from django.shortcuts import render
 from django.views import View, generic
 from .models import Product
 class HomeView(generic.ListView):
-    queryset = Product
+    queryset = Product.objects.filter(active=True)
     template_name = 'home/index.html'
     context_object_name = 'products'
     paginate_by = 5
+    
+class ProductDetail(generic.DetailView):
+    model = Product
+    template_name = 'home/product-details.html'
+    context_object_name = 'product'

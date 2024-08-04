b1f6c180ec5f0712af9118fed2faad26c8b99779
mohammadkhoddami <mohammadh.khoddami@gmail.com>
2024-08-04 15:33:08 +0330

    add comment admin to admin.py

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Comment
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('title', 'active', )
-    
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'product', 'body', 'status', 'stars')

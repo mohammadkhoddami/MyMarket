3c3c6a64ff0e6dacac6dfb97086e586f6e6c1152
mohammadkhoddami <mohammadh.khoddami@gmail.com>
2024-08-04 17:06:27 +0330

    create new url for comments

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,6 @@ app_name = 'home'
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
-    path('product/<int:pk>/', views.ProductDetail.as_view(), name='product')
+    path('product/<int:pk>/', views.ProductDetail.as_view(), name='product'),
+    path('comment/<int:pk>/', views.CommentCreateView.as_view(), name='comment'),
 ]

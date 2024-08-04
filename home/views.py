cdd247f8f42af66b46cc2736c2eb6f50ccf94615
mohammadkhoddami <mohammadh.khoddami@gmail.com>
2024-08-04 17:06:36 +0330

    create new view for createing comments

--- home/views.py
+++ home/views.py
@@ -1,7 +1,11 @@
 from typing import Any
-from django.shortcuts import render
+from django.forms import BaseModelForm
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 from django.views import View, generic
-from .models import Product
+from .models import Product, Comment
+from .forms import CommentForm
 class HomeView(generic.ListView):
     queryset = Product.objects.filter(active=True)
     template_name = 'home/index.html'
@@ -12,3 +16,25 @@ class ProductDetail(generic.DetailView):
     model = Product
     template_name = 'home/product-details.html'
     context_object_name = 'product'
+    
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['form'] = CommentForm()
+        return context
+        
+
+class CommentCreateView(generic.CreateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = 'home/product-details.html'
+    
+    def get_success_url(self):
+        return reverse('home:product', kwargs={'pk': self.kwargs['pk']})
+    
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.author = self.request.user
+        pk = int(self.kwargs['pk'])
+        product = get_object_or_404(Product, pk=pk)
+        obj.product = product
+        return super().form_valid(form)

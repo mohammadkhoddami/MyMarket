8afd574017722b7e2775c01a290152f07ee6d6e2
mohammadkhoddami <mohammadh.khoddami@gmail.com>
2024-08-04 20:13:23 +0330

    1- add deactive comments for better management 2- add commentinline for better management

--- home/admin.py
+++ home/admin.py
@@ -1,11 +1,26 @@
+from typing import Any
 from django.contrib import admin
+from django.db.models.query import QuerySet
+from django.http import HttpRequest
 from .models import Product, Comment
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    fields = ('author', 'body', 'status', 'stars')
+    extra = 1
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('title', 'active', )
-
+     
+    inlines = [
+         CommentInline
+     ]
 
 @admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
+class DeactiveCommentAdmin(admin.ModelAdmin):
     list_display = ('author', 'product', 'body', 'status', 'stars')
+    
+    def get_queryset(self, request: HttpRequest) -> QuerySet[Any]:
+        return super().get_queryset(request).filter(status=False)

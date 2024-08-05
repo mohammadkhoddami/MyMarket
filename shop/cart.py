f40b8d11daf168bb5e3494e0791b47e95ffb5b02
mohammadkhoddami <mohammadh.khoddami@gmail.com>
2024-08-06 00:38:19 +0330

    add delete to cart

--- shop/cart.py
+++ shop/cart.py
@@ -8,3 +8,21 @@ class Cart:
         if not cart:
             cart = self.session['cart'] = {}
             self.cart = cart
+            
+    def remove(self, product):
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart['product_id']
+            self.save()
+            
+    def add(self, product, quantity=1):
+        product_id = str(product.id)
+        
+        if product_id not in self.cart:
+            self.cart['product_id'] = {'quantity' : quantity}
+        else:
+            self.cart['product_id']['quantity'] += quantity
+        self.save()
+    
+    def save(self):
+        self.session.modified = True

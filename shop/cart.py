3beff356addb24f49e366f9cb931fb9fb3eaf3d0
mohammadkhoddami <mohammadh.khoddami@gmail.com>
2024-08-06 03:50:23 +0330

    add len method , get total price method and clear method

--- shop/cart.py
+++ shop/cart.py
@@ -1,4 +1,4 @@
-
+from home.models import Product
 
 class Cart:
     def __init__(self, request) -> None:
@@ -23,6 +23,31 @@ class Cart:
         else:
             self.cart['product_id']['quantity'] += quantity
         self.save()
+        
     
     def save(self):
         self.session.modified = True
+        
+    
+    def __iter__(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        cart = self.cart.copy()
+        
+        for product in products:
+            cart[str(product.id)]['product_obj'] = product
+        for item in cart.values():
+            yield item
+            
+    def __len__(self):
+        return len(self.cart.values())
+    
+    def clear(self):
+        del self.session['cart']
+        self.save()
+        
+    def get_total_price(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        
+        return sum([product.price for product in products])

7f16abc083eaaf51dc0c3c38fddc1a2f0217500a
mohammadkhoddami <mohammadh.khoddami@gmail.com>
2024-08-10 21:31:46 +0330

    just  make setting.py more beautiful

--- config/settings.py
+++ config/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     #local apps
     'accounts.apps.AccountsConfig',
     'home.apps.HomeConfig',

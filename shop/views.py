80ccf3bd6494a952894d10b5a7938b3a45593898
mohammadkhoddami <mohammadh.khoddami@gmail.com>
2024-08-10 21:32:49 +0330

    add views to cart view

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views import View, generic
+from .cart import Cart
+from .forms import AddToCartProdcutForm
+from home.models import Product
+class CartView(View):
+    def get(self, request):
+        cart = Cart(request)
+        return render(request, 'shop/cart.html', {'cart':cart})
 
-# Create your views here.
+
+class AddToCartView(View):
+    def post(self, request, product_id):
+        cart = Cart(request)
+        product = get_object_or_404(Product, pk=product_id)
+        form = AddToCartProdcutForm
+        if form.is_valid():
+            cd = form.cleaned_data
+            quantity = cd['quantity']
+            cart.add(product=product, quantity=quantity)
+        return redirect('shop:cart')
